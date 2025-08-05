4baa1fcf34bfce8f9da30cbdae2c34ab3c75c008
tarek el hojeiri <trhjr@Mac.Home>
2025-08-05 17:10:44 +0100

    Implement add bookmark functionality

--- src/app/components/bookmarks/bookmarks-create/bookmarks-create.component.ts
+++ src/app/components/bookmarks/bookmarks-create/bookmarks-create.component.ts
@@ -1,9 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, inject } from '@angular/core';
+import {
+  FormBuilder,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { BookmarksActions } from 'src/app/store/actions/bookmarks.actions';
 
 @Component({
   selector: 'app-bookmarks-create',
@@ -13,9 +24,44 @@ import { MatInputModule } from '@angular/material/input';
     MatFormFieldModule,
     MatInputModule,
     MatButtonModule,
+    MatCardModule,
+    MatIconModule,
+    MatDividerModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './bookmarks-create.component.html',
   styleUrl: './bookmarks-create.component.scss',
 })
-export class BookmarksCreateComponent {}
+export class BookmarksCreateComponent {
+  private fb = inject(FormBuilder);
+  private store = inject(Store);
+  private router = inject(Router);
+
+  form = this.fb.group({
+    title: ['', Validators.required],
+    url: [
+      '',
+      [
+        Validators.required,
+        Validators.pattern(
+          /^(https?:\/\/)?(www\.)?[\w.-]+\.[a-zA-Z]{2,}([\/\w .-]*)*\/?$/
+        ),
+      ],
+    ],
+  });
+
+  onSubmit() {
+    if (this.form.valid) {
+      const newBookmark = {
+        title: this.form.value.title!,
+        url: this.form.value.url!,
+        createdAt: new Date().toISOString(),
+      };
+      this.store.dispatch(
+        BookmarksActions.addBookmark({ bookmark: newBookmark })
+      );
+      this.router.navigate(['/']);
+    }
+  }
+}
